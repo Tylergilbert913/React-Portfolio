d7c882aaad6e87ce20e40420374fc684a5f9d928
Tyler Gilbert <tyler.gilbert913@gmail.com>
2021-05-10 13:45:43 -0400

    created the footer

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./footer.css";
+
+function footer() {
+    return (
+        <div className="fixed-bottom">
+            <center>
+                <footer>© 2021</footer>
+            </center>
+        </div>
+    )
+}
+
+export default footer;
