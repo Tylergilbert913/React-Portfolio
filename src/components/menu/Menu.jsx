016042e52feddafb6f250d0163d311ddd1ea6e6b
Tyler Gilbert <tyler.gilbert913@gmail.com>
2021-11-23 13:20:22 -0500

    added drop-menu for hamburger as a sidebar for navigation

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,8 +1,27 @@
-import React from "react";
-import "./Menu.scss";
+import "./menu.scss";
 
-function Menu() {
-  return <div></div>;
+function Menu({ menuOpen, setMenuOpen }) {
+  return (
+    <div className={"menu " + (menuOpen && "active")} >
+      <ul>
+      <li>
+          <a href="#intro">Home</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#works">My Work</a>
+        </li>
+        <li>
+          <a href="#testimonials">My Testimonials</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default Menu;
