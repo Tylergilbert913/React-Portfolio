3176c50085c9c67cb6ad5cbe22296823f309ae90
Tyler Gilbert <tyler.gilbert913@gmail.com>
2021-12-05 14:38:44 -0500

    fixed issue with importing images by using back ticks and $

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,32 +1,62 @@
 import "./works.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
+import { faPlug } from "@fortawesome/free-solid-svg-icons";
+import { faUtensils } from "@fortawesome/free-solid-svg-icons";
+import { faLeaf } from "@fortawesome/free-solid-svg-icons";
+import FeedMe from "../../images/FeedMe.png";
 import HomeGrown from "../../images/HomeGrown.png";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faChevronLeft } from '@fortawesome/free-solid-svg-icons'
-import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
-
-
-
+import ElectricAve from "../../images/ElectricAve.png";
 function Works() {
+  const data = [
+    {
+      id: "1",
+      icon: `${faUtensils}`,
+      title: "Feed Me",
+      desc: "Where will you eat next?",
+      img: `${FeedMe}`,
+      link: "https://github.com/Tylergilbert913/feed-me-tm",
+    },
+    {
+      id: "2",
+      icon: `${faLeaf}`,
+      title: "Home Grown",
+      desc: "For the modern day plant nurturer",
+      img: `${HomeGrown}`,
+      link: "https://github.com/caitie-gorham/project-2",
+    },
+    {
+      id: "3",
+      icon: `${faPlug}`,
+      title: "Electric Ave",
+      desc: "A simplfied solution for finding your next EV chargings station",
+      img: `${ElectricAve}`,
+      link: "https://github.com/skimmers/project_3",
+    },
+  ];
   return (
     <div className="works" id="works">
       <div className="slider">
-        <div className="container">
-          <div className="item">
-            <div className="left">
-              <div className="leftContainer">
-                <div className="imgContainer">
-                  <img src="" alt="" />
+        {data.map((d) => (
+          <div className="container">
+            <div className="item">
+              <div className="left">
+                <div className="leftContainer">
+                  <div className="imgContainer">
+                    <FontAwesomeIcon icon={d.icon} />
+                  </div>
+                  <h2>{d.title}</h2>
+                  <p>{d.desc}</p>
+                  <span>Projects</span>
                 </div>
-                <h2>Title</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adip</p>
-                <span>Projects</span>
+              </div>
+              <div className="right">
+                <img src={d.img} alt="" />
               </div>
             </div>
-            <div className="right">
-              <img src={HomeGrown} alt="" />
-            </div>
           </div>
-        </div>
+        ))}
       </div>
       <FontAwesomeIcon icon={faChevronLeft} className="arrow left" />
       <FontAwesomeIcon icon={faChevronRight} className="arrow right" />
--- src/data.js
+++ src/data.js
@@ -1,22 +1,25 @@
 import FeedMe from "./images/FeedMe.png";
+import HomeGrown from "./images/HomeGrown.png";
+import ElectricAve from "./images/ElectricAve.png";
+import RedWoods from "./images/Redwoods.jpg";
 
 export const featuredPortfolio = [
   {
     id: 1,
     title: "Feed Me",
-    img: {FeedMe},
+    img: `${FeedMe}`,
     link: "https://github.com/Tylergilbert913/feed-me-tm",
   },
   {
     id: 2,
     title: "Home Grown",
-    img: "images/HomeGrown.png",
+    img: `${HomeGrown}`,
     link: "https://github.com/caitie-gorham/project-2",
   },
   {
     id: 3,
     title: "Electric Ave",
-    img: "images/ElectricAve.png",
+    img: `${ElectricAve}`,
     link: "https://github.com/skimmers/project_3",
   },
 ];
@@ -25,18 +28,25 @@ export const webApplication = [
     {
         id: 1,
         title: "Feed Me",
-        img: "images/FeedMe.png",
+        img: `${FeedMe}`,
       },
       {
         id: 2,
         title: "Home Grown",
-        img: "images/HomeGrown.png",
+        img: `${HomeGrown}`,
       },
       {
         id: 3,
         title: "Electric Ave",
-        img: "images/ElectricAve.png",
+        img: `${ElectricAve}`,
       },
 ];
 
-export const photography = [{}];
+export const photography = [
+    {
+        id: 1,
+        title: "Tyler Gilbert",
+        img: `${RedWoods}`
+
+    }
+];
